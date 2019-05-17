0bc09803dcf46fe3d6be735ab7204ff813f2f519
Mr Robot <keisse.alexander@hotmail.com>
2019-05-17 13:41:33 +0200

    Feedback

--- src/be/intecbrussel/FileUtils.java
+++ src/be/intecbrussel/FileUtils.java
@@ -7,15 +7,40 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
 
+//TODO: Ik heb een .gitignore gemaakt met daarin de nodige excludes voor user/project specific files.
+// Deze zal de getrackte files niet hiden. Maar dan hebben jullie een template voor het volgende project.
+
+//TODO: Voor jdk 7 werd voor bestanden en mappen gebruikgemaakt van de klasse File.
+// Deze heeft evenwel minder mogelijkheden. Je kan deze gebruiken
+// maar het wordt aangeraden om voor nieuwe programma's gebruik te maken van de Path klasse.
+
+//TODO: Probeer je methoden in kleinere stukken op te delen,
+// je kan gebruik maken van private helper methoden daarvoor.
+
+//TODO: Probeer IOExceptions intern op te vangen in je Methoden en correct af te handelen.
+
 public class FileUtils {
 
-    /**list methode die een lijst teruggeeft van alle files in je directory en onderliggende directories;*/
+    //TODO: Door deze private constructor hide je de default public constructor
+    private FileUtils() {
+        super();
+    }
+
+    /**
+     * list methode die een lijst teruggeeft van alle files in je directory en onderliggende directories;
+     */
+    //TODO: Mooi gebruik van een recursive method
     public static void list(File folder, List<File> files) {
         if (!folder.isDirectory()) {
+            //TODO: Het if statement hierboven gaat na of het File object,
+            // een directory is, niet of die directory leeg is.
             System.out.println("Folder is empty");
         }
+
+        //TODO: Je kan hier ook gebruik maken van Files.list(folder.toPath())
+        // hierdoor krijg je een stream terug, en zou je eventueel met lambda's en foreach kunnen werken.
         File[] folderArray = folder.listFiles();
-        if (folderArray != null)
+        if (folderArray != null)  //TODO: {}, kan bugs voorkomen.
             for (File file : folderArray) {
                 if (file.isFile()) {
                     files.add(file);
@@ -26,21 +51,26 @@ public class FileUtils {
     }
 
 
-    /**geeft extension terug van de filename*/
+    /**
+     * geeft extension terug van de filename
+     */
     public static String getExtension(String filename) {
         String answer = null;
         if (filename == null) {
-            answer = null;
+            answer = null;  //TODO: Redundant code
         }
-        if (filename.contains(".")) {
+        if (filename.contains(".")) {  //TODO: if kan een NullPointerException geven!
+
             answer = filename.substring(filename.lastIndexOf(".") + 1);
         }
-        return answer;
+        return answer;  //TODO: De mogelijkheid bestaat dat je return value null is. Let hiermee op
     }
 
 
-    /**Gebruikt hashset om alle extensies in een set te gieten, daarna maak ik met deze set alle folders aan
-    Ten slotte giet ik alle bestanden in hun respectievelijke folder.*/
+    /**
+     * Gebruikt hashset om alle extensies in een set te gieten, daarna maak ik met deze set alle folders aan
+     * Ten slotte giet ik alle bestanden in hun respectievelijke folder.
+     */
     public static void createAndCopyDirectory(List<File> files, File directory) throws IOException {
         Set<String> newFiles = new HashSet<>();
         //automatische detectering van alle mogelijke extensies in de directory
@@ -48,7 +78,10 @@ public class FileUtils {
             newFiles.add(getExtension(file.getName()));
         }
         //aanmaak van nodige directories
+        //TODO: Je maakt hier in elke iteratie een nieuw object aan,
+        // beter zou zijn om 1 variabele aan te maken en die te hergebruiken.
         for (String s : newFiles) {
+            //TODO: Files.createDirectories(Paths.get(s)); zal hetzelfde effect hebben.
             if (!new File(directory + "/" + s).exists()) {
                 new File(directory + "/" + s).mkdir();
             }
@@ -56,7 +89,10 @@ public class FileUtils {
         //copieerwerk met check of de destination al bestaat, indien onbestaande wordt deze aangemaakt en volgegoten
         for (File file : files) {
             if (getExtension(file.getName()) != null) {
+                //TODO: Je kon hier de methode file.toPath() gebruiken.
                 Path source = Paths.get(file.getAbsolutePath());
+                //TODO: directory.toPath().resolve(getExtension(file.getName())).resolve(file.getName());
+                // Probeer op deze manier hard coded seperators te voorkomen.
                 Path destination = Paths.get(directory + "/" + getExtension(file.getName()) + "/" + file.getName());
                 if (!destination.toFile().exists()) {
                     Files.copy(source, destination);
@@ -66,7 +102,10 @@ public class FileUtils {
     }
 
 
-    /**Maakt een textfile op basis van de sorted directory*/
+    /**
+     * Maakt een textfile op basis van de sorted directory
+     */
+    //TODO: Deze methode heeft een te grote kost naar onderhoud toe
     public static void makeTextFile(File directory) throws IOException {
         List<File> sortedFiles = new ArrayList<>();
         FileUtils.list(directory, sortedFiles);
@@ -87,12 +126,14 @@ public class FileUtils {
             for (File file : sortedFiles) {
                 if (s.equals(file.getParentFile().getName())) {
                     summary.add(String.format("%-52s%5s%10s%5s%10s%5s%10s%5s", file.getName(), "|",
-                            file.canRead() ? "x" : "/", "|", file.canWrite() ? "x" : "/", "|",
+                            file.canRead() ? "x" : "/", "|",
+                            file.canWrite() ? "x" : "/", "|",
                             file.isHidden() ? "x" : "/", "|"));
                 }
             }
         }
         //checked of de summary al bestaat, indien niet wordt deze aangemaakt
+        //TODO: Je kon van directory + "/summary.txt" een final Path variabele gemaakt hebben.
         if (!Paths.get(directory + "/summary.txt").toFile().exists()) {
             Files.createFile(Paths.get(directory + "/summary.txt"));
         }
--- src/be/intecbrussel/mainApp.java
+++ src/be/intecbrussel/mainApp.java
@@ -5,15 +5,26 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+//TODO: Probeer consequent te zijn in de benamingen.
+// Ofwel ga je voor nederlandse benamingen of engelse[meer aangeraden],
+// maar niet door elkaar.
 
 public class mainApp {
+
+    //TODO: Er zit nog teveel logica in je main methode,
+    // probeer deze in je util klasse te houden.
     public static void main(String[] args) {
+
+        //TODO: Het lijkt me eenvoudiger om de ArrayList in de FileUtils klasse bij te houden.
         List<File> alleFiles = new ArrayList<>();
         //Hieronder kan je makkelijk source en destination aanpassen
-        File teSorterenDirectory = new File("c:/data/Unsorted/test 1");
+        //TODO: Probeer gebruik te maken van de Path klasse boven de File klasse[voor jdk 7]
+        File teSorterenDirectory = new File("C:/data/unsorted");
         File sortedDirectory = new File("c:/data/sorted");
+        //TODO: Je source en destination kan je beter onderbrengen in final variabelen.
 
         try {
+            //TODO: Je gebruikt hier de Javadoc je kon beter gebruik maken van single line comments
             /**Maakt een lijst van alle files in heel het onderliggende systeem*/
             FileUtils.list(teSorterenDirectory,alleFiles);
             /**Gebruikt de gemaakte lijst om directories aan te maken en alles te copiëren, originelen blijven behouden.*/
